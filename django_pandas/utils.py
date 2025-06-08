28f79ab0f6d43cf1282f8e37254fcbb7f1bc9922
RicardoDdePaula <85744079+RicardoDdePaula@users.noreply.github.com>
2025-06-08 19:09:32 -0300

    Update utils.py

--- django_pandas/utils.py
+++ django_pandas/utils.py
@@ -40,26 +40,41 @@ def invalidate_signal_handler(sender, **kwargs):
     invalidate(kwargs['instance'])
 
 
-def replace_pk(model):
+def get_to_field(field):
+    tof = field.to_fields[0] if field.to_fields else None
+    return tof or 'pk'
+
+
+def replace_fk(model, to_field='pk'):
     base_cache_key = get_base_cache_key(model)
 
-    def get_cache_key_from_pk(pk):
-        return None if pk is None else base_cache_key % int(pk)
+    def get_cache_key_from_value(val):
+        if val is None:
+            return None
+        else:
+            try:
+                intVal = int(val)
+                return base_cache_key % intVal
+            except:
+                return base_cache_key % val
 
-    def inner(pk_series):
-        pk_series = pk_series.astype(object).where(pk_series.notnull(), None)
-        cache_keys = pk_series.apply(get_cache_key_from_pk)
+    def inner(value_series):
+        value_series = value_series.astype(object).where(value_series.notnull(), None)
+        cache_keys = value_series.apply(get_cache_key_from_value)
         unique_cache_keys = list(filter(None, cache_keys.unique()))
 
         if not unique_cache_keys:
-            return pk_series
+            return value_series
 
         out_dict = cache.get_many(unique_cache_keys)
 
         if len(out_dict) < len(unique_cache_keys):
-            out_dict = dict([(base_cache_key % obj.pk, force_text(obj))
-                            for obj in model.objects.filter(
-                            pk__in=list(filter(None, pk_series.unique())))])
+            values = list(filter(None, value_series.unique()))
+            objs = model.objects.filter(**{f"{to_field}__in": values})
+            out_dict = {
+                base_cache_key % getattr(obj, to_field): force_text(obj)
+                for obj in objs
+            }
             cache.set_many(out_dict)
 
         return list(map(out_dict.get, cache_keys))
@@ -78,7 +93,7 @@ def build_update_functions(fieldnames, fields):
                 yield fieldname, replace_from_choices(choices)
 
             elif field and field.get_internal_type() == 'ForeignKey':
-                yield fieldname, replace_pk(get_related_model(field))
+                yield fieldname, replace_fk(get_related_model(field), to_field=get_to_field(field))
 
 
 def update_with_verbose(df, fieldnames, fields):
